30145ea2f1691e269b781a781a741528c5697559
yaxing.chen <chenyaxing@rt-thread.com>
2020-12-05 20:20:25 +0800

    add rt smart kernel

---
+++ rt_smart_project_0/rtconfig_preinc.h
@@ -0,0 +1,12 @@
+
+#ifndef RTCONFIG_PREINC_H__
+#define RTCONFIG_PREINC_H__
+
+/* Automatically generated file; DO NOT EDIT. */
+/* RT-Thread pre-include file */
+
+#define HAVE_CCONFIG_H
+#define _STDC_PREDEF_H
+#define __STDC_ISO_10646__ 201206L
+
+#endif /*RTCONFIG_PREINC_H__*/
